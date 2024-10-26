8c285ebb8bb3bcad084f4cdafb526516aa58f9f4
Patrick Mahinge <109906110+affilirator@users.noreply.github.com>
2024-10-26 22:01:45 +0300

    Properly Format Tags

--- src/frontmatter/tags.js
+++ src/frontmatter/tags.js
@@ -1,4 +1,4 @@
-// get array of decoded tag names
+/* get array of decoded tag names
 module.exports = (post) => {
 	if (!post.data.category) {
 		return [];
@@ -9,4 +9,26 @@ module.exports = (post) => {
 		.map(({ $: attributes }) => decodeURIComponent(attributes.nicename));
 
 	return categories;
+};
+*/
+// Function to capitalize the first letter of each word and replace hyphens with spaces
+const formatTagName = (tag) => {
+  return tag
+    .split("-") // Split by hyphen
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize first letter
+    .join(" "); // Join back with a space
+};
+
+// Get array of decoded tag names
+module.exports = (post) => {
+  if (!post.data.category) {
+    return [];
+  }
+
+  const categories = post.data.category
+    .filter((category) => category.$.domain === "post_tag")
+    .map(({ $: attributes }) => decodeURIComponent(attributes.nicename));
+
+  // Format categories to the desired style
+  return categories.map(formatTagName);
 };
